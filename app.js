8ffd015397c38128e73fedc9f9343d5422baf4f2
Gaston Recalde <gastonrg94@gmail.com>
2022-11-24 15:29:38 -0300

    Proyecto listo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hola');
+const input = document.getElementById('email');
+const form = document.getElementById('form');
+const small = document.getElementById('error');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!regEmail.test(input.value) ){
+        small.className = 'email__small';
+        input.classList.add('email__invalid');
+        input.placeholder = 'email@example.com';
+    } else {
+        small.className = 'small';
+        input.classList.remove('email__invalid');
+    }
+});
